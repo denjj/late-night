dac703c259ccffee8a39754d2a85020a5cc69685
Dennis <dennis.jim@outlook.com>
2020-04-04 19:03:48 -0700

    Added volume slider

--- scripts/audio-controls.js
+++ scripts/audio-controls.js
@@ -1,15 +1,30 @@
+"use strict";
 
-  let widgetIframe = document.getElementById('sc-widget');
-  let widget = SC.Widget(widgetIframe);
+// Initialize iframe SC Widget
+let widgetIframe = document.getElementById('sc-widget');
+let widget = SC.Widget(widgetIframe);
 
-  document.getElementById("play-pause").addEventListener("click", function(){
-    widget.toggle();
-  });
+// Add Event listeners for media buttons
+document.getElementById("play-pause").addEventListener("click", function(){
+  widget.toggle();
+});
 
-  document.getElementById("prev").addEventListener("click", function(){
-    widget.prev();
-  });
+document.getElementById("prev").addEventListener("click", function(){
+  widget.prev();
+});
 
-  document.getElementById("next").addEventListener("click", function(){
-    widget.next();
-  });
+document.getElementById("next").addEventListener("click", function(){
+  widget.next();
+});
+
+// Volume Slider
+var slider = document.getElementById("volumeSlider");
+var output = document.getElementById("volumeVal");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+  widget.setVolume(this.value);
+}
+
+// Track Seeker
