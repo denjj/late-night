bd0c16972807dadddfc9ab08cebbf7702e9aa076
Dennis <dennis.jim@outlook.com>
2020-04-06 16:44:52 -0700

    Added track-seeker control slider

--- scripts/audio-controls.js
+++ scripts/audio-controls.js
@@ -18,13 +18,34 @@ document.getElementById("next").addEventListener("click", function(){
 });
 
 // Volume Slider
-var slider = document.getElementById("volumeSlider");
-var output = document.getElementById("volumeVal");
-output.innerHTML = slider.value;
+var volumeSlider = document.getElementById("volumeSlider");
+var volumeVal = document.getElementById("volumeVal");
+volumeVal.innerHTML = volumeSlider.value;
 
-slider.oninput = function() {
-  output.innerHTML = this.value;
+volumeSlider.oninput = function() {
+  volumeVal.innerHTML = this.value;
   widget.setVolume(this.value);
 }
 
 // Track Seeker
+var seekSlider = document.getElementById("seekSlider");
+var seekVal = document.getElementById("seekVal");
+seekVal.innerHTML = seekSlider.value;
+
+seekSlider.oninput = function() {
+  seekVal.innerHTML = this.value;
+  widget.seekTo(this.value);
+}
+
+// Track Movement
+widget.bind(SC.Widget.Events.PLAY_PROGRESS, function(){
+  widget.getPosition(function(position){
+    let slide = document.getElementById("seekSlider");
+    widget.getDuration(function(trackLength) {
+      let slideMax = document.getElementById("seekSlider");
+      slideMax.max = trackLength;
+    });
+    slide.value = position;
+  });
+})
+
